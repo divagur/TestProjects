1ac66e3b2994615c094fd288e53d756a5cdd4ebb
Vladimir <vladimir.zhuravlev@bdrthermea.ru>
2024-04-12 12:40:39 +0300

    изменить скрипт расчета

--- PlanningTask/скрипт_расчета_пополнения.sql
+++ PlanningTask/скрипт_расчета_пополнения.sql
@@ -4,6 +4,7 @@ FROM
 inner join LV_OrderShipItem with (nolock) ON LV_OrderShipItemStock.oss_OrderShipItemID = LV_OrderShipItem.osi_ID 
 inner join LV_OrderShipment with (nolock) ON LV_OrderShipItem.osi_OrderShipmentID = LV_OrderShipment.ost_ID
 inner join LV_Order with (nolock) on ord_id = ost_OrderID
-left join LV_TaskDependency with (nolock) on tkd_TaskID = oss_TaskID
+inner join LV_TaskDependency with (nolock) on tkd_TaskID = oss_TaskID
+inner join LV_Task with (nolock) on tsk_ID = tkd_PrereqTaskID  and tsk_StatusID in (1,2)
 where ord_StatusID <> 3
 group by ost_OrderID, ord_code

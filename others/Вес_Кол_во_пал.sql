b7ddf48bfed75bc9dcb52e6c7060c92ec683ce21
Vladimir <vladimir.zhuravlev@bdrthermea.ru>
2024-06-05 15:03:21 +0300

    Альтернативный расчет вес и кол-ва паллет заказа

---
+++ others/Вес_Кол_во_пал.sql
@@ -0,0 +1,30 @@
+
+
+select distinct
+		ord_ID
+		, (shc_ID) 
+		, (isnull(a1.TotNetWeght, a.TotNetWeght) ) totNetWeght
+		, (isnull(a1.PalWeght, a.PalWeght) ) PalWeght
+from
+		LV_Order with (nolock)
+join	LV_OrderShipment with (nolock) on ost_OrderID = ord_ID
+join	LV_ShipContainer with (nolock) on ost_id =  shc_OrderShipmentID
+left join 	(
+			select shs_ProductID, shs_ContainerID ContainerID,  (shs_CUQuantity * ipt_NetWeight) TotNetWeght, sdp_DeadWeight PalWeght
+			from
+					LV_ShipStock
+			join	LV_ItemPackType with (nolock) on ipt_ID = shs_CUItemUnitID
+			join	LV_ShipContainer with (nolock) on shc_id = shs_ContainerID
+			join V_StandardPackType  with(nolock) on  sdp_UnitID = shc_UnitID
+		)a on a.ContainerID = shc_ID
+
+left join 	(
+			select stk_ProductID, stk_ContainerID ContainerID, ( stk_CUQuantity * ipt_NetWeight) TotNetWeght, sdp_DeadWeight PalWeght 
+			from
+					LV_Stock
+			join	LV_ItemPackType with (nolock) on ipt_ID = stk_CUItemUnitID
+			join	LV_StockContainer with (nolock) on stc_ID = stk_ContainerID
+			join V_StandardPackType  with(nolock) on  sdp_UnitID = stc_UnitID
+		) a1 on a1.ContainerID = shc_ContainerID
+where
+		ord_id =  45281

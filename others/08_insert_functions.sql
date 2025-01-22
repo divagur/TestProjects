1e2dfe2a79fb3fafbd8840d06a16c990cabd42f5
Vladimir <vladimir.zhuravlev@bdrthermea.ru>
2025-01-22 14:11:22 +0300

    логи

--- others/08_insert_functions.sql
+++
@@ -1,3 +0,0 @@
-insert into functions(id,name) values('Warehouse','Склады')
-insert into functions(id,name) values('CustomPost','Таможенные посты')
-insert into functions(id,name) values('TransporView','Виды транспорта')

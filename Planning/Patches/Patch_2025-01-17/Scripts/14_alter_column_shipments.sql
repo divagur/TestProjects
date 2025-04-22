aee6b26d27f661d6e8e788a41fdbf0c600a11e65
dzhuravlev <dzhuravlev@almazantey.ru>
2025-04-22 09:06:39 +0300

    Изменение длин колонок

---
+++ Planning/Patches/Patch_2025-01-17/Scripts/14_alter_column_shipments.sql
@@ -0,0 +1,3 @@
+alter table shipment_invoices ALTER COLUMN  number varchar(32) null
+alter table shipments ALTER COLUMN  vehicle_number varchar(32) null
+alter table shipments ALTER COLUMN  trailer_number varchar(32) null

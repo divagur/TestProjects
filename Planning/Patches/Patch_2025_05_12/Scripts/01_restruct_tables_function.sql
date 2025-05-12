cba2ff06f0929f5f8c7033c17277a09028db5a60
dzhuravlev <dzhuravlev@almazantey.ru>
2025-05-12 16:56:39 +0300

    Разработка

---
+++ Planning/Patches/Patch_2025_05_12/Scripts/01_restruct_tables_function.sql
@@ -0,0 +1,23 @@
+CREATE TABLE pl_functions
+(
+    id int IDENTITY (1,1) NOT NULL,
+	code varchar(50) null,
+    name varchar(120) null
+	CONSTRAINT [PK_pl_functions] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY];
+
+GO
+
+insert into pl_functions (code, name)
+select id, name from functions
+
+go 
+
+exec sp_rename 'dbo.functions', 'functions_ex'
+
+go 
+
+exec sp_rename 'dbo.pl_functions', 'functions'
